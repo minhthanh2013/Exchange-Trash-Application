27cb919409dfa804be4742786bb6be30e2c39b64
unknown <you@example.com>
2021-01-18 22:35:09 +0700

    third commit

---
+++ node_modules/react-native-camera/android/build/generated/source/buildConfig/general/debug/org/reactnative/camera/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.reactnative.camera;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "org.reactnative.camera";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "org.reactnative.camera";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "general";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
